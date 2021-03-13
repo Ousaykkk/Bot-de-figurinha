836486a5ac04d7dc6c6a8c0cad177df13bd65287
ousaykkk <ousaybelfegor@gmail.com>
2021-03-12 23:25:29 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const ousaymenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Ousay:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *OUSAY*
+  DUVIDAS?
+  WA.me/5584988433046
+╚════════════════════`
+}
+
+exports.ousaymenu = ousaymenu
+
+
+
+
+
+
+
+
